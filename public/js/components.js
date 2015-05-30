743ff9b952c0e254d659b1623d7936d0a0c44acc
Josh Hornby <hello@joshhornby.co.uk>
2015-05-30 10:45:56 +0100

    first commit

---
+++ public/js/components.js
@@ -0,0 +1,9 @@
+var Message = React.createClass({displayName: "Message",
+	render: function () {
+		return (
+			React.createElement("div", {className: "commentBox"}, 
+				"Hello, world! I am a CommentBox."
+			)
+		);
+	}
+});
---
+++ resources/assets/js/components.jsx
@@ -0,0 +1,9 @@
+var Message = React.createClass({
+	render: function () {
+		return (
+			<div className="commentBox">
+				Hello, world! I am a CommentBox.
+			</div>
+		);
+	}
+});
